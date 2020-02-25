ad16839a86c72b08db3e69cf013d4d92d27c1d76
Maxime Salomon <maxime@croissant.io>
2020-02-25 17:15:00 +0100

    events routes wip

--- client/components/EventList.js
+++ client/components/EventList.js
@@ -1,5 +1,6 @@
 import { useQuery } from "@apollo/react-hooks";
 import gql from "graphql-tag";
+import Link from "next/link";
 
 export const ALL_EVENTS_QUERY = gql`
   query allEvents {
@@ -29,17 +30,22 @@ const EventList = () => {
 };
 
 export function EventCard({ event }) {
+  const event_link = "/events/" + event.name
   return (
-    <div class="flex-none w-80 px-4">
-      <div key={event.id} className="rounded overflow-hidden shadow-lg">
-        <img className="h-48 w-full" src={event.poster} />
-        <div className="px-4 py-4 bg-gray-700 h-32">
-          <div className="font-bold text-white text-lg mb-2">{event.name}</div>
-          <p className="text-gray-200 font-bold text-sm">{event.date}</p>
-          <p className="text-gray-500 text-sm">{event.location}</p>
+    <Link href={event_link}>
+      <div class="flex-none w-80 px-4">
+        <div key={event.id} className="rounded overflow-hidden shadow-lg">
+          <img className="h-48 w-full" src={event.poster} />
+          <div className="px-4 py-4 bg-gray-700 h-32">
+            <div className="font-bold text-white text-lg mb-2">
+              {event.name}
+            </div>
+            <p className="text-gray-200 font-bold text-sm">{event.date}</p>
+            <p className="text-gray-500 text-sm">{event.location}</p>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
