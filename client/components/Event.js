38fc2ea552a77900068ea9fda099bcfbd07dffb6
Maxime Salomon <maxime@croissant.io>
2020-02-27 22:16:41 +0100

    wip design event page

--- client/components/Event.js
+++ client/components/Event.js
@@ -1,13 +1,17 @@
 const Event = ({ event }) => {
   return (
     <header>
-      <section className="pt-32 bg-gray-900 h-screen text-center">
-        <h1 className="pt-8 text-4xl text-white font-bold">{event.name}</h1>
-        <img className="pt-8 max-h-64 m-auto" src={event.poster} />
-        <div className="text-white">
-          <p className="text-lg font-bold">Informations :</p>
-          <p>Date : {event.date}</p>
-          <p>Lieu : {event.location}</p>
+      <section className="pt-32 bg-gray-900 h-screen">
+        <div className="max-w-4xl m-auto">
+          <h1 className="m-auto pt-8 text-4xl text-white font-bold">
+            {event.name}
+          </h1>
+          <img className="pt-12" src={event.poster} />
+          <div className="pt-6 text-white">
+            <p className="text-xl font-bold">Informations :</p>
+            <p className="pt-2"><span className="font-bold">Date :</span> {event.date}</p>
+            <p><span className="font-bold">Lieu :</span> {event.location}</p>
+          </div>
         </div>
       </section>
     </header>
