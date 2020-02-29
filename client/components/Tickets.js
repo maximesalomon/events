a882871d66697f14aba83530855793f5326d972c
Maxime Salomon <maxime@croissant.io>
2020-02-29 14:23:41 +0100

    ticket cart wip

--- client/components/Tickets.js
+++ client/components/Tickets.js
@@ -5,18 +5,27 @@ const Tickets = () => {
         <p className="text-xl font-bold text-white">Billets</p>
         <div className="flex justify-between mt-4">
           <div className="bg-white w-3/5 rounded flex justify-between">
-            <p className="text-lg font-bold pl-8 py-6">Normal</p>
+            <div className="flex items-center">
+              <p className="text-lg font-bold pl-8 py-6">Normal</p>
+              <p className="text-sm text-gray-700 pl-8 py-6">20,00€</p>
+            </div>
             <div className="text-base py-6 pr-12 flex">
               <button className="rounded-full h-8 w-8 flex items-center justify-center bg-shotgun-pink hover:bg-purple-700 text-white">
                 -
               </button>
-              <div className="px-4 pt-1 text-lg font-bold">1</div>
+              <div className="px-4 pt-1 text-lg font-bold">0</div>
               <button className="rounded-full h-8 w-8 flex items-center justify-center bg-shotgun-pink hover:bg-purple-700 text-white">
                 +
               </button>
             </div>
           </div>
-          <div className="bg-white w-2/6 rounded">2</div>
+          <div className="bg-white w-2/6 rounded">
+            <div className="flex">
+              <p className="p-4 text-lg font-bold">
+                <span>0 </span>billet séléctionné
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </>
