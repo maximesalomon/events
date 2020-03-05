959bc414c9a7590ac8d12736747c0c37cefcb948
Maxime Salomon <maxime@croissant.io>
2020-03-05 22:47:35 +0100

    wip

--- client/components/Hero.js
+++ client/components/Hero.js
@@ -11,11 +11,18 @@ const Hero = () => {
         <p className="text-gray-400 font-base">
           Une programmation au summum de l'éclectisme et de la qualité !
         </p>
-        <Link as={"/events/marvellous-island-festival-2020-d0e32dbd-15b6-423c-a4b1-0a8d25814f3c"} href="/events/[eventNameId]"><a>
-          <button className="mt-6 text-white bg-shotgun-pink hover:bg-purple-700 py-3 px-10 rounded">
-            Découvrir
-          </button>
-        </a></Link>
+        <Link
+          as={
+            "/events/marvellous-island-festival-2020-d0e32dbd-15b6-423c-a4b1-0a8d25814f3c"
+          }
+          href="/events/[eventNameId]"
+        >
+          <a>
+            <button className="mt-6 text-white bg-shotgun-pink hover:bg-purple-700 py-3 px-10 rounded">
+              Découvrir
+            </button>
+          </a>
+        </Link>
       </section>
     </header>
   );
